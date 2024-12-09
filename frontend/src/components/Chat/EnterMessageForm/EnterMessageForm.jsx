8b0782fe7e1fec2bdc38ebed3062d22b02dabb25
Nina Zyabrina <zyabrina95@gmail.com>
2024-12-09 19:44:18 +0100

    add server.js and redo enterMessForm

--- frontend/src/components/Chat/EnterMessageForm/EnterMessageForm.jsx
+++ frontend/src/components/Chat/EnterMessageForm/EnterMessageForm.jsx
@@ -1,17 +1,7 @@
 import { SendButton } from './Buttons.jsx';
 import _ from 'lodash';
-import React, { useRef, useState } from "react";
-// import { updateMessagesArray } from '../utils.js';
-import state from '../state.js'
-
-
-const updateMessagesArray = (e) => {
-  const [input] = e.target.children;
-  if (input.value.length > 0) {
-    state.messages.push({id: _.uniqueId, message: input.value});
-    return state;
-  };
-};
+import React, { useRef, useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 const MessageComponent = ({ userName, message }) => {
   return (
@@ -22,36 +12,53 @@ const MessageComponent = ({ userName, message }) => {
 };
 
 const EnterMessageForm = () => {
-  const [index, setIndex] = useState(0);
-  const [components, setComponents] = useState([]);
-  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [socket, setSocket] = useState(null);
 
+  useEffect(() => {
+    const socket = io("http://localhost:5002");
+    setSocket(socket);
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); 
-    updateMessagesArray(e)
+    
+    socket.on("messages", (msgs) => {
+      setMessages(msgs);
+    });
 
-    setComponents(prevComponents => [
-        ...prevComponents,
-        <MessageComponent userName='admin' key={state.messages[index].id} message={state.messages[index].message} />
-      ]);
-      setIndex(prevIndex => prevIndex + 1);
-    setInputText('')
-};
+    socket.on("message", (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
 
-const handleInputChange = (event) => {
-    setInputText(event.target.value);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (newMessage.trim() !== "") {
+      socket.emit("sendMessage", newMessage);
+      setNewMessage("");
+    }
   };
+
   return (
     <>
-    <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-      {components}
-    </div>
-    <form noValidate="" className="py-1 border rounded-2" onSubmit={handleSubmit}>
+      {messages.map((msg, index) => (
+        <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+          {/* пусть пока с индексом будет, потом переделать в ид сообщение */}
+          <MessageComponent userName='admin' key={index} message={msg} />
+          </div>
+        ))}
+    <form noValidate="" className="py-1 border rounded-2" onSubmit={handleSendMessage}>
       <input 
-        name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2 form-control"
-        value={inputText} 
-        onChange={handleInputChange} />
+        name="body" 
+        aria-label="Новое сообщение" 
+        placeholder="Введите сообщение..." 
+        className="border-0 p-0 ps-2 form-control"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+      />
       <SendButton />
     </form>
     </>
