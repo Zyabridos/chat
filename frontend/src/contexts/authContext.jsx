dbf704a34385b82f1826f30c9aca67b9a74efadb
Nina Zyabrina <zyabrina95@gmail.com>
2025-01-24 17:04:57 +0100

    remove useMemo

--- frontend/src/contexts/authContext.jsx
+++ frontend/src/contexts/authContext.jsx
@@ -4,6 +4,7 @@ import React, {
   useState,
   useContext,
   useEffect,
+  useCallback,
 } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -27,39 +28,44 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
+  const { t } = useTranslation();
   const [user, setUser] = useState(null);
   const [serverError, setServerError] = useState(null);
   const navigate = useNavigate();
-  const { t } = useTranslation();
 
   useEffect(() => {
     const { user: storedUser } = getUserAndTokenFromStorage();
     if (storedUser) {
       setUser(storedUser);
+      navigate(routes.mainPage());
     }
-  }, []);
+  }, [navigate]);
 
-  const logIn = async (login, password, setErrorMessage, setAuthFailed) => {
-    try {
-      const response = await axios.post(routes.loginPath(), { username: login, password });
-      if (response?.data) {
-        const { token, username } = response.data;
-        const userData = { token, username };
-        saveUserToStorage(userData);
-        setUser(userData);
-        navigate(routes.mainPage());
+  // recreate funcs (logIn, logOut, signUp) only when dependecies changes (navigate, t, dispatch)
+  const logIn = useCallback(
+    async (login, password, setErrorMessage, setAuthFailed) => {
+      try {
+        const response = await axios.post(routes.loginPath(), { username: login, password });
+        if (response && response.data) {
+          const { token, username } = response.data;
+          const userData = { token, username };
+          saveUserToStorage(userData);
+          setUser(userData);
+          navigate(routes.mainPage());
+        }
+        return response;
+      } catch (error) {
+        handleLoginErrors(error, t, setErrorMessage, setAuthFailed);
       }
-      return response;
-    } catch (error) {
-      handleLoginErrors(error, t, setErrorMessage, setAuthFailed);
-    }
-  };
+    },
+    [navigate, t],
+  );
 
-  const logOut = () => {
+  const logOut = useCallback(() => {
     removeUserFromStorage();
     setUser(null);
     navigate(routes.loginPage());
-  };
+  }, [navigate]);
 
   const signUp = async (login, password) => {
     try {
@@ -67,8 +73,10 @@ const AuthProvider = ({ children }) => {
       if (response?.data) {
         const { token, username } = response.data;
         const userData = { token, username };
+        
         saveUserToStorage(userData);
         setUser(userData);
+
         navigate(routes.mainPage());
       }
       return response;
@@ -83,3 +91,5 @@ const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export default AuthProvider;
